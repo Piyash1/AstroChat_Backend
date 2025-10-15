39821beeaedd02e8359181ee407304810bb2aa15
Piyash1 <moniruzzamanpiyash@gmail.com>
2025-10-15 17:42:00 +0600

    Updated allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'astrochat-backend-a0os.onrender.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://astrochat-backend-a0os.onrender.com']
 
 
 # Application definition
